c4e1334084fe3d9e0adf671f6a01fe3b5fa6d3f0
busi-n <khulisanimsipa@gmail.com>
2026-03-12 16:58:28 +0200

    added variables

---
+++ input output activity 2.0/app/src/main/java/com/example/inputoutputactivity20/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.inputoutputactivity20
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // get the button using the id we set on the user interface
+        val clickMeButton = findViewById<Button>(R.id.clickMeButton)
+        val displayTxt = findViewById<TextView>(R.id.displayTxt)
+        val nameTxt = findViewById<EditText>(R.id.nameTxt)
+        
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
