e1a3d49eba0bf6943c19aba47a19998de11f82b8
busi-n <khulisanimsipa@gmail.com>
2026-03-17 12:30:46 +0200

    switch added

--- input output activity 2.0/app/src/main/java/com/example/inputoutputactivity20/MainActivity.kt
+++ input output activity 2.0/app/src/main/java/com/example/inputoutputactivity20/MainActivity.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,10 +21,24 @@ class MainActivity : AppCompatActivity() {
 
         // get the button using the id we set on the user interface
         val clickMeButton = findViewById<Button>(R.id.clickMeButton)
-        val displayTxt = findViewById<TextView>(R.id.displayTxt)
+        var displayTxt =findViewById<TextView>(R.id.displayTxt)
         val nameTxt = findViewById<EditText>(R.id.nameTxt)
-        
+        val zuluSwitch = findViewById<Switch>(R.id.zuluSwitch)
 
+        //add code to the button that happens when its clicked
+        clickMeButton?.setOnClickListener {
+            var greeting: String
+            if (zuluSwitch.isChecked) {
+                greeting = "Sawubona,${nameTxt.text}!"
+            }else{
+                greeting = "Welcome,${nameTxt.text}!"
+            }
+            displayTxt.text = greeting
+           Toast.makeText(this@MainActivity,
+               "Button clicked", Toast.LENGTH_LONG).show()
+            displayTxt.text = "Welcome,${nameTxt.text}!"
+
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
